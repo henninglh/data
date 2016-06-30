b8f1bf9af2226c82ff2fb8247209f42a7c78bb5c
Henning Lund-Hanssen <hennilu@student.matnat.uio.no>
2016-06-30 16:37:51 +0200

    Combining scores is done

--- scoring/combine.py
+++ scoring/combine.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python
 import sys
 
-dragon_filename = sys.argv[1]
-disgen_filename = sys.argv[2]
+if len(sys.argv) < 4:
+    sys.exit('./combine.py <gene-file> <score-file> <output-file>')
+    exit()
+
+genes_filename = sys.argv[1]
+scores_filename = sys.argv[2]
 combined_filename = sys.argv[3]
 
-with open(dragon_filename, 'r') as dragon,\
-        open(disgen_filename, 'r') as disgen,\
+with open(genes_filename, 'r') as genes,\
+        open(scores_filename, 'r') as scores,\
         open(combined_filename, 'w') as combined:
 
-    dragon.readline() # remove header
-    disgen.readline() # remove header
-    approved_genes = set((line.rstrip() for line in dragon.readlines()))
+    genes.readline() # remove header
+    scores.readline() # remove header
+    approved_genes = set((line.rstrip() for line in genes.readlines()))
 
-    for line in disgen.readlines():
+    for line in scores.readlines():
         gene_name, score  = line.split('\t')
 
         if gene_name in approved_genes:
