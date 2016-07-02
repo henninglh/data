f208f04c32cf05ada984caae59d3a72890b3394d
Henning Lund-Hanssen <henning.lund.hanssen@gmail.com>
2016-07-02 20:19:04 +0200

    diffing now only looks at clusters with scores larger than zero...disappointing results....

--- results/diff.py
+++ results/diff.py
@@ -8,10 +8,17 @@ with open(filename + '_results.tsv', 'r') as results_file, \
         open(filename + '_scores.tsv', 'r') as scores_file, \
         open(filename + '-FINAL.txt', 'w') as finals:
 
-    results = [cluster.split('\t')[0].strip() for cluster in
-               results_file.readlines()]
-    scores = [cluster.split('\t')[0].strip() for cluster in
-              scores_file.readlines()]
+    results = []
+    for line in results_file.readlines():
+        info = line.split('\t')
+        line = info[0].strip()
+        score = float(info[1].strip())
+
+        if score > 0.0:
+            results.append(line)
+
+    scores = [line.split('\t')[0].strip() for line in scores_file.readlines()]
+    scores = scores[:len(results)]
 
     matcher = SequenceMatcher(None, results, scores)
     diff = matcher.ratio()
