fba7e007af0839828fda56e986107b5081c98dce
Henning Lund-Hanssen <henning.lund.hanssen@gmail.com>
2016-07-15 00:25:20 +0200

    Added 109K file

--- network/clean_all.py
+++ network/clean_all.py
@@ -5,7 +5,7 @@ import pandas as pd
 mitab_name = 'mitab_lite.tsv'
 
 # Clean MITAB-MINI file from iRefWeb
-mitab_table = pd.read_table('mitab_lite_50073.txt')
+mitab_table = pd.read_table('mitab_lite_109276.txt')
 mitab_table.to_csv(mitab_name, sep='\t', columns=['a_alias', 'b_alias'],
         index=False)
 
