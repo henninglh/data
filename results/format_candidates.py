6d911934a891847a2cf52807a99d5f0f871911dc
Henning Lund-Hanssen <henning.lund.hanssen@gmail.com>
2016-07-15 01:35:52 +0200

    Changed format_candidates.py to only list the candidates - to query CCGD

--- results/format_candidates.py
+++ results/format_candidates.py
@@ -11,12 +11,16 @@ with open('candidates.tsv', 'r') as candidates,\
         info = line.split('\t')
         if len(info) == 3 and len(info[2].strip()) > 0:
             g = info[2].strip()
-            diff = len(g.split(','))
-            genes.append([diff,g])
-            singles += diff
+            #diff = len(g.split(','))
+            #genes.append([diff,g])
+            #singles += diff
+            genes.append(g)
+            singles += len(g.split(','))
 
 
-    cand_only.write('candidates_count\tcandidates\ttotal:{}\n'.format(singles))
+    #cand_only.write('candidates_count\tcandidates\ttotal:{}\n'.format(singles))
+    cand_only.write('candidates\ttotal:{}\n'.format(singles))
 
     for gene in genes:
-        cand_only.write('{}\t{}\n'.format(gene[0], gene[1]))
+        #cand_only.write('{}\t{}\n'.format(gene[0], gene[1]))
+        cand_only.write('{}\n'.format(gene))
