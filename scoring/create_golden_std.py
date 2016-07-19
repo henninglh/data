85e26f5ba7b2bbcc2bd8cb5219db28fbcf25223e
Henning Lund-Hanssen <hennilu@student.matnat.uio.no>
2016-07-19 21:37:31 +0200

    Another golden standard change

--- scoring/create_golden_std.py
+++ scoring/create_golden_std.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import sys
+
 with open('disgen_clean.tsv', 'r') as disgen,\
         open('dragon_clean.txt', 'r') as dragon,\
         open('golden_standard.tsv', 'w') as golden_standard:
@@ -8,18 +10,24 @@ with open('disgen_clean.tsv', 'r') as disgen,\
     dragon.readline()
     golden_standard.write('gene_names\tscore\n')
 
+
     markers = dict()
 
     for line in disgen.readlines():
         gene, score = line.split('\t')
-        markers[gene] = float(0.25)
+        markers[gene] = float(score)
+
+    maximum = max(markers.itervalues())
+    minimum = min(markers.itervalues())
+    average = (maximum + minimum) / 2
+    sane_max = average + ((maximum - average) / 4)
 
     for line in dragon.readlines():
         gene = line.strip()
-        if gene in markers:
-            markers[gene] += float(0.25)
+        if gene not in markers:
+            markers[gene] = sane_max
         else:
-            markers[gene] = float(0.25)
+            markers[gene] = minimum
 
     for marker in markers:
         golden_standard.write('{}\t{}\n'.format(marker, markers[marker]))
