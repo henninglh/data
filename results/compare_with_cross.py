f6cf7b56e1ca718a703063f6a3c2ae6de2e76a72
Henning Lund-Hanssen <henning.lund.hanssen@gmail.com>
2016-07-20 18:30:31 +0200

    Starting with cross validation comparison

---
+++ results/compare_with_cross.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import sys
+
+with open('RESULTS-PLACEHOLDER', 'r') as res1,\
+        open('../scoring/cross_validation.tsv', 'r') as res2,\
+        open('cross_results.txt', 'w') as results:
+
+    
+
