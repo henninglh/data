6fce2b1dbe2ca1693604190dc54c61f7cbcc94b8
Henning Lund-Hanssen <henning.lund.hanssen@gmail.com>
2016-07-21 01:44:09 +0200

    Tests complete

--- results/compare_with_cross.py
+++ results/compare_with_cross.py
@@ -4,19 +4,31 @@ import sys
 
 with open('clusters.tsv', 'r') as clean,\
         open('../scoring/cross_validation.txt', 'r') as cross_validation,\
-        open('cross_results.txt', 'w') as result:
+        open('cross_validation_matched.txt', 'w') as matched,\
+        open('cross_validation_unmatched.txt', 'w') as unmatched:
 
     clean.readline()
     cross_validation.readline()
+    matched.write('gene_names\n')
     clean_genes = [line for line in clean.readlines()]
     cross_val_genes = set([line.strip() for line in cross_validation.readlines()]) 
+    identified_genes = set()
     hits = 0
 
     for line in clean_genes:
         info = line.split('\t')
         genes = set(info[2].split(','))
-        intersect = cross_val_genes.intersection(genes)
-        hits += len(cross_val_genes.intersection(genes))
+        intersect = list(cross_val_genes.intersection(genes))
+        map(lambda x: identified_genes.add(x), intersect)
+        hits += len(intersect)
+
+    for gene in identified_genes:
+        matched.write('{}\n'.format(gene))
+
+    for gene in (cross_val_genes - identified_genes):
+        unmatched.write('{}\n'.format(gene))
 
     # Percentage of hits
-    print float(hits) / float(len(cross_val_genes))
+    print 'total possible matches:', len(cross_val_genes)
+    print 'matches:', hits
+    print 'percentage:',float(hits) / float(len(cross_val_genes))
