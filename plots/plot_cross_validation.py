579fbce18bd4aed6c84f3f9b66605280c8b31247
Henning Lund-Hanssen <henning.lund.hanssen@gmail.com>
2016-07-25 15:21:09 +0200

    Why was this not committed before?

--- plots/plot_cross_validation.py
+++ plots/plot_cross_validation.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 
+import matplotlib.pyplot as plt
+    
+found_in_cluster = list()
+possible_in_cluster = list()
+
 with open('cross_validation.txt', 'r') as possible,\
         open('cv_20_percent.tsv', 'r') as found:
 
@@ -10,12 +15,37 @@ with open('cross_validation.txt', 'r') as possible,\
     identified = 0
     possibles = 0
 
+
     for line in found.readlines():
         info = line.split('\t')
+        cluster = int(float(info[0].strip()))
         genes_with_scores = info[2].split(',')
         genes = set(tuple(map(lambda x: x.split(':')[0], genes_with_scores)))
-        identified += len(possible_genes.intersection(genes))
+        genes_identified = possible_genes.intersection(genes)
+        identified += len(genes_identified)
         possibles += len(genes)
+
+        found_in_cluster.append(len(genes_identified))
+        possible_in_cluster.append(len(genes))
 
     print 'Percentage found in the top 20% genes:',((float(identified)
         / float(possibles)) * 100)
+
+print found_in_cluster
+print len(found_in_cluster)
+
+ax = plt.subplot(111)
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.get_xaxis().tick_bottom()
+ax.get_yaxis().tick_left()
+
+plt.xticks(range(0, len(found_in_cluster) + 1, 1), fontsize=14)
+plt.yticks(range(0, 5, 1), fontsize=14)
+
+plt.title('Genes in top 20 percent that was removed by cross validation', fontsize=20)
+plt.xlabel('Cluster rank', fontsize=16)
+plt.ylabel('Number of genes', fontsize=16)
+
+plt.hist(found_in_cluster, color='#3F5D7D')
+plt.savefig('removed_by_cv.png', bbox_inches='tight')
