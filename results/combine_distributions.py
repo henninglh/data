09fd28929dae3e3fa80543f309247a84318c9ef9
Henning Lund-Hanssen <henning.lund.hanssen@gmail.com>
2016-07-27 17:14:11 +0200

    Distribution done - on to plotting

--- results/combine_distributions.py
+++ results/combine_distributions.py
@@ -6,7 +6,7 @@ import numpy as np
 dist_files = sys.argv[1:]
 
 distributions = []
-ranks = np.zeros((10,440))
+ranks = np.zeros((10,440), dtype=np.int64)  # 440 is amount of clusters
 
 for idx, dist in enumerate(dist_files):
     with open(dist.strip(), 'r') as distribution:
@@ -15,3 +15,8 @@ for idx, dist in enumerate(dist_files):
             ranks[idx][rank-1] = count
 
 print ranks.sum(axis=0)
+rank = 1
+with open('cv_total_dist.tsv', 'w') as full_dist:
+    for frequency in ranks.sum(axis=0):
+        full_dist.write('{}\t{}\n'.format(rank, frequency))
+        rank += 1
