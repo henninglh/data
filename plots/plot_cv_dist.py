f38e930bdef7cc3d213d204301850b384166e27e
Henning Lund-Hanssen <henning.lund.hanssen@gmail.com>
2016-07-28 17:29:14 +0200

    Cleanup

--- plots/plot_cv_dist.py
+++ plots/plot_cv_dist.py
@@ -5,5 +5,5 @@ import matplotlib as plt
 distribution = []
 with open('../results/cv_total_dist.tsv', 'r') as dist_file:
     distribution = [int(i.split('\t')[1].strip()) for i in dist_file.readlines()]
-print distribution[8]
+
 
