a3225c2a41c1b2e29a1e1fc155d00c5ad181538b
Henning Lund-Hanssen <hennilu@student.matnat.uio.no>
2016-07-29 21:44:28 +0200

    Cancer network ranked

--- results/clean.py
+++ results/clean.py
@@ -16,6 +16,7 @@ cols.remove('selected')
 for score in scorings:
     cols.remove(score.strip())
 table.fillna(value=0)
+#table = table.sort_values(by=['PRWP', '__mclCluster', 'name'], ascending=[False,
 table = table.sort_values(by=['MAA', '__mclCluster', 'name'], ascending=[False,
     True, True])
 table.to_csv(clean_filename, na_rep=0.0, index=False, sep='\t',
--- results/create_clusters.py
+++ results/create_clusters.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 
 from collections import OrderedDict
+import sys
+
+f = sys.argv[1].strip()
 
 #with open('mcl-1.8_prwp_a-8_i-1000_links-43706_mean_clean.tsv', 'r') as clean,\
-with open('PRWP/mcl-1.8_prwp_a-3_i-30_links-43706_golden_clean.tsv', 'r') as clean,\
+#with open('PRWP/mcl-1.8_prwp_a-3_i-30_links-43706_golden_clean.tsv', 'r') as clean,\
+#with open('PRWP/mcl-1.8_prwp_a-3_i-30_links-487_golden_cancer_clean.tsv', 'r') as clean,\
 #with open('MAA/mcl-1.8_maa_links-43706_golden_clean.tsv', 'r') as clean,\
+#with open('MAA/mcl-1.8_maa_links-4487_golden_cancer_clean.tsv', 'r') as clean,\
+with open(f, 'r') as clean,\
         open('clusters.tsv', 'w') as cluster_file:
     clean.readline()
 
