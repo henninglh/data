0c1ead2ed946dc2493ec8b9a58c51b4e46de1576
Henning Lund-Hanssen <henning.lund.hanssen@gmail.com>
2016-07-30 20:16:59 +0200

    Cluster results done

--- results/create_clusters.py
+++ results/create_clusters.py
@@ -4,14 +4,16 @@ from collections import OrderedDict
 import sys
 
 f = sys.argv[1].strip()
-
+d = f.split('/')[0] + '/'
+num = sys.argv[2].strip()
 #with open('mcl-1.8_prwp_a-8_i-1000_links-43706_mean_clean.tsv', 'r') as clean,\
 #with open('PRWP/mcl-1.8_prwp_a-3_i-30_links-43706_golden_clean.tsv', 'r') as clean,\
 #with open('PRWP/mcl-1.8_prwp_a-3_i-30_links-487_golden_cancer_clean.tsv', 'r') as clean,\
 #with open('MAA/mcl-1.8_maa_links-43706_golden_clean.tsv', 'r') as clean,\
 #with open('MAA/mcl-1.8_maa_links-4487_golden_cancer_clean.tsv', 'r') as clean,\
+        #open('clusters.tsv', 'w') as cluster_file:
 with open(f, 'r') as clean,\
-        open('clusters.tsv', 'w') as cluster_file:
+        open(d + 'clusters_cancer' + num + '.tsv', 'w') as cluster_file:
     clean.readline()
 
     cluster_file.write('clusters\tscore\tgenes\n')
