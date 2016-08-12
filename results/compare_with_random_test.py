0cd917ef657b57a63a07941d24b7242ecd1c9ba4
Henning Lund-Hanssen <henning.lund.hanssen@gmail.com>
2016-08-12 19:47:05 +0200

    Added lethal files and changed comparison script

--- results/compare_with_random_test.py
+++ results/compare_with_random_test.py
@@ -6,35 +6,40 @@ test = sys.argv[2]
 
 with open(d + 'clusters_full.tsv', 'r') as clean,\
         open(test, 'r') as test_set,\
-        open(d + 'lethal_final.tsv', 'w') as core:
+        open(d + 'movember_final.tsv', 'w') as core:
 
     clean.readline()
     test_set.readline()
     clean_genes = filter(lambda x: float(x.split('\t')[1]) > 0.0, 
             [line for line in clean.readlines()])
     test_genes = set([line.strip() for line in test_set.readlines()]) 
-    core.write('rank\tbiomarkers\tcandidates\tcluster\n')
+    core.write('rank\ttest biomarkers\ttest candidates\tremaining biomarkers\tremaining candidates\n')
 
     rank = 1
     for line in clean_genes:
         info = line.split('\t')
-        genes = set([i.split(':')[0].strip() for i in info[2].split(',')])
+        biomarkers = set([a.split(':')[0] for a in 
+                filter(lambda x: float(x.split(':')[1].strip()) != 0.0, 
+                [i for i in info[2].split(',')])])
         candidates = set([a.split(':')[0] for a in 
                 filter(lambda x: float(x.split(':')[1].strip()) == 0.0, 
                 [i for i in info[2].split(',')])])
-        biomarkers = test_genes.intersection(genes - candidates)
+        tested = test_genes.intersection(biomarkers)
         candids = test_genes.intersection(candidates)
-        ident_non = ','.join(genes - candids - biomarkers)
-        ident_marker = ','.join(biomarkers)
-        ident_candid = ','.join(candids)
+        test_marker = ','.join(tested)
+        test_cand = ','.join(candids)
+        non_test_marker = ','.join(biomarkers - tested)
+        non_test_cand = ','.join(candidates - candids)
 
-        if (len(ident_marker) + len(ident_candid)) > 0:
-            if len(ident_non) == 0:
-                ident_non = '-'
-            if len(ident_marker) == 0:
-                ident_marker = '-'
-            if len(ident_candid) == 0:
-                ident_candid = '-'
-            core.write('{}\t{}\t{}\t{}\n'.format(rank, ident_marker, ident_candid,
-                ident_non))
+        if (len(test_marker) + len(test_cand)) > 0:
+            if len(non_test_marker) == 0:
+                non_test_marker = '-'
+            if len(non_test_cand) == 0:
+                non_test_cand = '-'
+            if len(test_marker) == 0:
+                test_marker = '-'
+            if len(test_cand) == 0:
+                test_cand = '-'
+            core.write('{}\t{}\t{}\t{}\t{}\n'.format(rank, test_marker, test_cand,
+                non_test_marker, non_test_cand))
         rank += 1
