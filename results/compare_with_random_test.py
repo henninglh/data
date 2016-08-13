82288c1c566437e0745e1d3262dcc26935ae4dc1
Henning Lund-Hanssen <henning.lund.hanssen@gmail.com>
2016-08-13 04:29:37 +0200

    Now outputs the amount of matches

--- results/compare_with_random_test.py
+++ results/compare_with_random_test.py
@@ -6,13 +6,14 @@ test = sys.argv[2]
 
 with open(d + 'clusters_full.tsv', 'r') as clean,\
         open(test, 'r') as test_set,\
-        open(d + 'movember_final.tsv', 'w') as core:
+        open(d + 'lethal_final.tsv', 'w') as core:
 
     clean.readline()
     test_set.readline()
     clean_genes = filter(lambda x: float(x.split('\t')[1]) > 0.0, 
             [line for line in clean.readlines()])
     test_genes = set([line.strip() for line in test_set.readlines()]) 
+    matched = 0
     core.write('rank\ttest biomarkers\ttest candidates\tremaining biomarkers\tremaining candidates\n')
 
     rank = 1
@@ -26,6 +27,7 @@ with open(d + 'clusters_full.tsv', 'r') as clean,\
                 [i for i in info[2].split(',')])])
         tested = test_genes.intersection(biomarkers)
         candids = test_genes.intersection(candidates)
+        matched += len(tested) + len(candids)
         test_marker = ','.join(tested)
         test_cand = ','.join(candids)
         non_test_marker = ','.join(biomarkers - tested)
@@ -43,3 +45,4 @@ with open(d + 'clusters_full.tsv', 'r') as clean,\
             core.write('{}\t{}\t{}\t{}\t{}\n'.format(rank, test_marker, test_cand,
                 non_test_marker, non_test_cand))
         rank += 1
+    print matched
